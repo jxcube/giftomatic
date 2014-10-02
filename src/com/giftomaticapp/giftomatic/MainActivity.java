b67f2ed8ae356a12d55ee99904adfc24004e6a21
coderay1100 <rayp1100@gmail.com>
2014-10-02 18:21:31 +0700

    define the behavior of clicking email sign up button and log in button

--- src/com/giftomaticapp/giftomatic/MainActivity.java
+++ src/com/giftomaticapp/giftomatic/MainActivity.java
@@ -7,23 +7,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 
 public class MainActivity extends Activity {
 	
 	private ImageButton emailBtn;
+	private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        // Get the reference to the (sign up with) email
+        // button and define the behavior when the user clicks it
         emailBtn = (ImageButton) findViewById(R.id.email_btn);
         emailBtn.setOnClickListener(new OnClickListener() {
         	@Override
         	public void onClick(View v) {
+        		// When it is clicked, give user the registration form
         		startActivity(new Intent(MainActivity.this, SignUp.class));
+        	}
+        });
+        
+        // Get the reference to the Login button
+        // and define the behavior when the user clicks it
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		// When it is clicked, go to the login page
+        		startActivity(new Intent(MainActivity.this, LoginPage.class));
         	}
         });
     }
