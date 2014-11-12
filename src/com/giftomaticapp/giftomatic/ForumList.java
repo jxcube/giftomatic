0b18f1c55e887f12db6bee683fb3a7f9c0543972
Kusumo <shirokamiyuuto@gmail.com>
2014-11-12 09:16:01 +0000

    initialization

--- src/com/giftomaticapp/giftomatic/ForumList.java
+++ src/com/giftomaticapp/giftomatic/ForumList.java
@@ -28,7 +28,7 @@ public class ForumList extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_forum_list);
-		
+		getThread();
 	}
 
 	@Override
