c99d2fa8999f0e9713f8dea02eeed0d3c0263852
Muhammad Zulfikar <izuleonel@gmail.com>
2014-11-27 13:18:25 +0700

    filter main view
    add tags content

--- src/com/giftomaticapp/giftomatic/Filter.java
+++ src/com/giftomaticapp/giftomatic/Filter.java
@@ -1,77 +1,99 @@
 package com.giftomaticapp.giftomatic;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import butterknife.InjectView;
-import butterknife.OnClick;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.AdapterView.OnItemClickListener;
-
 public class Filter extends Activity {
+
+	GridView gridView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_filter);
-		// LinkedList<String> Tags = new LinkedList();
-		// getTagsData();
 
+		// request items
+		getItemDataFiltered("apalh");
 	}
-
-	@OnClick(R.id.technology)
-	public void getFiltered() {
-
-	}
-
-	// private void getTagsData() {
-	// String url = "http://api.giftomaticapp.com/tags";
-	// JsonArrayRequest request = new JsonArrayRequest(url, new
-	// Response.Listener<JSONArray>() {
-	// @Override
-	// public void onResponse(JSONArray response) {
-	// try {
-	// List<String> tags = new ArrayList<String>();
-	// for (int i = 0; i < response.length(); i++) {
-	// JSONObject item = response.getJSONObject(i);
-	// tags.add(item.toString);
-	// }
-	// requestTags(items);
-	// } catch (JSONException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// }, new Response.ErrorListener() {
-	// @Override
-	// public void onErrorResponse(VolleyError err) {
-	//
-	// }
-	// });
-	// NetworkSingleton.getInstance(this).addToRequestQueue(request);
-	// }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.filter, menu);
+		getMenuInflater().inflate(R.menu.gift_gallery, menu);
+
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		// int id = item.getItemId();
+		// if (id == R.id.action_settings) {
+		// return true;
+		// }
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void requestItems(final List<String> tags) {
+		// TODO
+		CustomTags adapter = new CustomTags(this, tags);
+		gridView = (GridView) findViewById(R.id.tags);
+		gridView.setAdapter(adapter);
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Intent intent = new Intent(Filter.this, ItemDetail.class);
+				intent.putExtra("itemId", items.get(position).id);
+				startActivity(intent);
+			}
+		});
+	}
+
+	public void getItemDataFiltered(String s) {
+		String url = "http://api.giftomaticapp.com/item" + s;
+		JsonArrayRequest request = new JsonArrayRequest(url,
+				new Response.Listener<JSONArray>() {
+					@Override
+					public void onResponse(JSONArray response) {
+						try {
+							List<Item> items = new ArrayList<Item>();
+							for (int i = 0; i < response.length(); i++) {
+								JSONObject item = response.getJSONObject(i);
+								items.add(new Item(item));
+							}
+							requestItems(items);
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+				}, new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError err) {
+
+					}
+				});
+		NetworkSingleton.getInstance(this).addToRequestQueue(request);
+	}
 }
